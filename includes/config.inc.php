92c86f184321db30ddb5b3337ad9b09ea7410821
prolti88 <prolti@hotmail.com>
2024-05-09 15:29:17 +0200

    Update config.inc.php
    aloldal név módosítások

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -8,14 +8,14 @@ $lablec = array('copyright'=>'Copyright '.date("Y").'.','owner'=>' - AT IT Servi
 
 $oldalak = array(
     '/' =>array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
-    'bejelentkezes' =>array('fajl' => 'bejelentkezes', 'szoveg' => 'Bejelentkezés', 'menun' => array(1,1)),
-    'kijelentkezes' =>array('fajl' => 'kijelentkezes', 'szoveg' => 'Kijelentkezés', 'menun' => array(1,1)),
+    'bejelentkezes' => array('fajl' => 'bejelentkezes', 'szoveg' => 'Bejelentkezés', 'menun' => array(1,0)),
+    'kijelentkezes' => array('fajl' => 'kijelentkezes', 'szoveg' => 'Kijelentkezés', 'menun' => array(0,1)),
     'bejelentkezve' => array('fajl' => 'bejelentkezve', 'szoveg' => '', 'menun' => array(0,0)),
     'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0)),
     'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek' , 'menun' => array(1,1)),
-    'kapcsolat' =>array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menu' => 1),
-    'uzenet' =>array('fajl' => 'uzenet', 'szoveg' => 'Üzenet', 'menun' => array(1,1)),
-    'uzenet_tablazat' =>array('fajl' => 'uzenet_tablazat', 'szoveg' => 'Üzenetek', 'menun' => array(1,1)),
+    'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'hibajegy' => array('fajl' => 'hibajegy', 'szoveg' => 'Hibajegy', 'menun' => array(1,1)),
+    'hibajegyek' => array('fajl' => 'hibajegyek', 'szoveg' => 'Hibajegyek', 'menun' => array(0,1)),
     );
 
 $hiba_oldal = array('fajl'=>'404','szoveg'=>'A keresett oldal nem található!','menun' => array(1,1));
