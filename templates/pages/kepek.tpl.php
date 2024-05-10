1618fdc97efcebd05e052d109f20f91a644cd75d
tthdvd88 <tthdvd88@gmail.com>
2024-05-10 07:29:40 +0200

    fooldal / kepek / kepfeltolt
    Fooldal --> video beagyazas
    Kepek --> galeria
    Kepfeltolt.php --> include galeriaba

--- templates/pages/kepek.tpl.php
+++ templates/pages/kepek.tpl.php
@@ -1,1 +1,50 @@
-<h1>Ez a Galéria</h1>
+
+
+<?php
+    $MAPPA = './images/';
+    $TIPUSOK = array ('.jpg', '.png');
+    $MEDIATIPUSOK = array('image/jpeg', 'image/png');
+    $MAXMERET = 500*1024;
+?>
+
+<?php
+$kepek = array();
+    $olvaso = opendir($MAPPA);
+    while (($fajl = readdir($olvaso)) !== false)
+        if (is_file($MAPPA.$fajl)) {
+            $vege = strtolower(substr($fajl, strlen($fajl)-4));
+            if (in_array($vege, $TIPUSOK))
+                $kepek[$fajl] = filemtime($MAPPA.$fajl);            
+        }
+    closedir($olvaso);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+<div id="kepek">
+    <div id="galeria">
+    <h2>Referencia munkák</h2>
+    <?php
+    // arsort($kepek);
+    foreach($kepek as $fajl => $datum)
+    {
+    ?>
+        <div class="kep">
+            <a href="<?php echo $MAPPA.$fajl ?>">
+                <img src="<?php echo $MAPPA.$fajl ?>">
+            </a>            
+        </div>
+    <?php
+    }
+    ?>
+    </div>
+
+    <?php
+    include('kepfeltolt.tpl.php');
+    ?>
+</div>
+
+</body>
+</html>
